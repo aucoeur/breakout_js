86b7bf159e760a41fb89b655edc3738502bab79c
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-03-31 10:33:08 -0700

    Step 3: Bounce off the walls

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-    	// JavaScript code goes here
-	const canvas = document.getElementById("myCanvas");
-	const ctx = canvas.getContext("2d");
+// JavaScript code goes here
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
-	let x = canvas.width / 2;
-	let y = canvas.height - 30;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
-	let dx = 2;
-	let dy = -2;
+let dx = 2;
+let dy = -2;
 
-	function drawBall() {
-	    ctx.beginPath();
-	    ctx.arc(x, y, 10, 0, Math.PI * 2);
-	    ctx.fillStyle = "#0095DD";
-	    ctx.fill();
-	    ctx.closePath();
-	}
+let ballRadius = 10;
 
-	function draw() {
-	    ctx.clearRect(0, 0, canvas.width, canvas.height);
-	    drawBall();
-	    x += dx;
-	    y += dy;
-	}
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-	setInterval(draw, 10);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    // For left/right canvas walls
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    // For top/bottom canvas walls
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
